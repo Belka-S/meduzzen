44a25383c94fd971d6744981686186286be48004
Belka-S <Belka-S@users.noreply.github.com>
2024-04-12 23:58:29 +0000

    deploy: bd4a0ab441b2408195b5ec0a6705389bb742c973

--- assets/index-CMdWmW6Y.js
+++
@@ -1,1 +0,0 @@
-import{j as e,T as a}from"./index-efMiHanE.js";const p=s=>{const{className:t="",fontWeight:o=700,children:r}=s;return e.jsx(a,{className:t,el:"h5",fontWeight:o,children:r})};export{p as H};
--- assets/index-D5kOB87F.js
+++
@@ -1,1 +0,0 @@
-import{j as e,T as a}from"./index-efMiHanE.js";const p=s=>{const{className:t="",fontWeight:o=700,children:r}=s;return e.jsx(a,{className:t,el:"h1",fontWeight:o,children:r})};export{p as H};
