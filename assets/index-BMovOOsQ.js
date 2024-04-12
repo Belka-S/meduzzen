746806a545ab820282cf1a90d77c844637eec555
Belka-S <Belka-S@users.noreply.github.com>
2024-04-12 23:06:41 +0000

    deploy: 22450afed0b772175044c902da1f4e330705d195

--- assets/index-BMovOOsQ.js
+++
@@ -1,1 +0,0 @@
-import{j as e,T as a}from"./index-ynwh1Inb.js";const p=s=>{const{className:t="",fontWeight:o=700,children:r}=s;return e.jsx(a,{className:t,el:"h5",fontWeight:o,children:r})};export{p as H};
